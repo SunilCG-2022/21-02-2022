5d412a013d624ed47bddc8491df2fe1da929316f
SunilG-Git <92194789+SunilG-Git@users.noreply.github.com>
2022-02-21 17:38:33 +0530

    first commit gitbash

---
+++ GitConnection/src/CountryName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CountryName {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your Country Name:");
+
+		Scanner sc=new Scanner(System.in);
+
+		String s=sc.nextLine();
+		
+		
+	}
+
+}
